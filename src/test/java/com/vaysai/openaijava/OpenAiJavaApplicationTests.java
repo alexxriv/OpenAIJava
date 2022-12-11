493290fd1e8ac2fc089e8a249f99051035cf28e3
alexxriv <alex.trrzrivera@gmail.com>
2022-12-11 14:09:35 -0600

    license added

--- src/test/java/com/vaysai/openaijava/OpenAiJavaApplicationTests.java
+++ src/test/java/com/vaysai/openaijava/OpenAiJavaApplicationTests.java
@@ -4,14 +4,10 @@ package com.vaysai.openaijava;
 import com.vaysai.openaijava.client.OpenAIClient;
 import com.vaysai.openaijava.model.models.ListModelsResponse;
 import lombok.extern.log4j.Log4j2;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @Log4j2
-@RunWith(SpringRunner.class)
+
 public class OpenAiJavaApplicationTests {
 
 	@Autowired
@@ -20,7 +16,7 @@ public class OpenAiJavaApplicationTests {
 
 	}
 
-	@Test
+
 	public void listModels(){
 		ListModelsResponse listModelsResponse=  openAIClient.listModels();
 		log.info(listModelsResponse);
