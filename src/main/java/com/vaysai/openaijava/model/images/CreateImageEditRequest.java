c5542aba59cd42bd81caa018df64f951480dbcf7
alexxriv <alex.trrzrivera@gmail.com>
2022-12-12 03:19:48 -0600

    tests for images

--- src/main/java/com/vaysai/openaijava/model/images/CreateImageEditRequest.java
+++ src/main/java/com/vaysai/openaijava/model/images/CreateImageEditRequest.java
@@ -2,6 +2,7 @@ package com.vaysai.openaijava.model.images;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Builder
 @Data
@@ -14,14 +15,14 @@ public class CreateImageEditRequest {
      *     A text description of the desired image(s). The maximum length is 1000 characters.
      */
     @NonNull
-    private String image;
+    private MultipartFile image;
 
     /**
      *     An additional image whose fully transparent areas
      *     (e.g. where alpha is zero) indicate where image should be edited.
      *     Must be a valid PNG file, less than 4MB, and have the same dimensions as image.
      */
-    private String mask;
+    private MultipartFile mask;
 
     /**
      *     A text description of the desired image(s). The maximum length is 1000 characters.
@@ -32,8 +33,7 @@ public class CreateImageEditRequest {
     /**
      * The number of images to generate. Must be between 1 and 10.
      */
-    @JsonProperty("number_of_images")
-    private Integer numberOfImages;
+    private Integer n;
 
     /**
      * The format in which the generated images are returned. Must be one of url or b64_json
@@ -43,8 +43,7 @@ public class CreateImageEditRequest {
     /**
      * The format in which the generated images are returned. Must be one of url or b64_json
      */
-    @JsonProperty("response_format")
-    private String responseFormat;
+    private String response_format;
 
     /**
      * A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse. Learn more.
