1eb89586d8a74c824658593d3b79931c3b926048
Erickarroyo18 <erickarroyo@ciencias.unam.mx>
2022-12-12 22:29:30 -0600

    [stage]Completed @Test

--- src/test/java/com/vaysai/openaijava/OpenAiJavaApplicationTests.java
+++ src/test/java/com/vaysai/openaijava/OpenAiJavaApplicationTests.java
@@ -11,6 +11,7 @@ import com.vaysai.openaijava.model.edits.CreateEditResponse;
 import com.vaysai.openaijava.model.embeddings.CreateEmbeddingsRequest;
 import com.vaysai.openaijava.model.embeddings.CreateEmbeddingsResponse;
 import com.vaysai.openaijava.model.files.*;
+import com.vaysai.openaijava.model.finetunes.*;
 import com.vaysai.openaijava.model.images.*;
 import com.vaysai.openaijava.model.models.ListModelsResponse;
 import com.vaysai.openaijava.model.models.RetrieveModelResponse;
@@ -37,186 +38,149 @@ import java.io.IOException;
 @ContextConfiguration(classes = {YamlPropertySourceFactory.class})
 class OpenAiJavaApplicationTests {
 
+    @Autowired
+    OpenAIClient openAIClient;
+    @Test
+    void listModels() {
+        ListModelsResponse listModelsResponse = openAIClient.listModels();
+        log.info(listModelsResponse);
+    }
 
-	@Autowired
-	OpenAIClient openAIClient;
+    @Test
+    void retrieveModel() {
+        RetrieveModelResponse retrieveModelResponse = openAIClient.retrieveModel("text-davinci-003");
+        log.info(retrieveModelResponse);
+    }
+    @Test
+    void createCompletion() {
+        CreateCompletionRequest createCompletionRequest = CreateCompletionRequest
+                .builder()
+                .model("text-davinci-003")
+                .prompt("Say this is a test")
+                .maxTokens(7)
+                .temperature(0.0).build();
 
-	@Test
-	void listModels(){
+        CreateCompletionResponse createCompletionResponse = openAIClient.createCompletion(createCompletionRequest);
+        log.info(createCompletionResponse.getChoices().get(0).getText());
+    }
+    @Test
+    void createEdit() {
+        CreateEditRequest request = CreateEditRequest.builder()
+                .model("text-davinci-edit-001")
+                .input("What day of the wek is it?")
+                .instruction("Fix the spelling mistakes")
+                .numberOfEdits(1)
+                .build();
+        CreateEditResponse createEditResponse = openAIClient.createEdit(request);
+        log.info(createEditResponse);
+    }
+    @Test
+    void createImage() {
+        CreateImageRequest request = CreateImageRequest.builder()
+                .prompt("A cute baby sea otter")
+                .numberOfImages(2)
+                .size("1024x1024").build();
+        CreateImageResponse response = openAIClient.createImage(request);
+        log.info(response);
+    }
 
-		ListModelsResponse listModelsResponse = openAIClient.listModels();
-		log.info(listModelsResponse);
-	}
+    @Test
+    void createImageEdit() throws IOException {
+        MultipartFile image = new MockMultipartFile("image.png", "image.png", "image/png", new FileInputStream(new File("src/test/resources/image.png")));
+        MultipartFile mask = new MockMultipartFile("mask.png", "mask.png", "image/png", new FileInputStream(new File("src/test/resources/mask.png")));
+        Assertions.assertNotNull(image);
+        //Assertions.assertNotNull(mask);
+        CreateImageEditRequest request = CreateImageEditRequest.builder()
+                .image(image)
+                .mask(mask)
+                .prompt("A golden apple in the middle of space")
+                .size("1024x1024")
+                .build();
+        CreateImageEditResponse response = openAIClient.createImageEdit(request);
+        log.info(response);
+    }
+    @Test
+    void createImageVariation() throws IOException {
+        MultipartFile image = new MockMultipartFile("image.png", "image.png", "image/png", new FileInputStream(new File("src/test/resources/image.png")));
+        CreateImageVariationRequest request = CreateImageVariationRequest.builder()
+                .image(image)
+                .n(2)
+                .size("1024x1024")
+                .build();
+        CreateImageVariationResponse response = openAIClient.createdImageVariation(request);
+        log.info(response);
+    }
+    @Test
+    void createEmbeddings() {
+        CreateEmbeddingsRequest request = CreateEmbeddingsRequest.builder()
+                .model("text-similarity-babbage-001")
+                .input("The food was delicious and the waiter was in a good mood")
+                .build();
+        CreateEmbeddingsResponse response = openAIClient.createEmbeddings(request);
+        log.info(response);
+    }
+    @Test
+    void listFiles() {
+        ListFilesResponse response = openAIClient.listFiles();
+        log.info(response);
+    }
+    @Test
+    void uploadFile() throws IOException {
+        MultipartFile image = new MockMultipartFile("data.jsonl", "data.jsonl", "application/x-ndjson", new FileInputStream(new File("src/test/resources/data.jsonl")));
+        UploadFileRequest request = UploadFileRequest.builder()
+                .file(image)
+                .purpose("fine-tune")
+                .build();
+        UploadFilesResponse response = openAIClient.uploadFile(request);
+        log.info(response);
+    }
+    @Test
+    void deleteFile() {
+        DeleteFileResponse response = openAIClient.deleteFile("file-qaD6okdiNT7p3siM3zFfGo5Q");
+        log.info(response);
+    }
+    @Test
+    void retrieveFile() {
+        RetrieveFileResponse response = openAIClient.retrieveFile("file-HcNL83etYjsrhpE1HwKMIq8e");
+        log.info(response);
+    }
+    @Test
+    void retrieveFileContent() {
+        ResponseEntity<?> response = openAIClient.retrieveFileContent("file-HcNL83etYjsrhpE1HwKMIq8e");
+        log.info(response);
+    }
+    @Test
+    void createFineTune() {
+        CreateFineTuneRequest request = CreateFineTuneRequest.builder().trainingFile("file-HcNL83etYjsrhpE1HwKMIq8e").build();
+        CreateFineTuneResponse response = openAIClient.createFineTune(request);
+        log.info(response);
+    }
+    @Test
+    void listFineTune() {
+        ListFineTuneResponse response = openAIClient.listFineTunes();
+        log.info(response);
+    }
+    @Test
+    void retrieveFineTune() {
+        RetrieveFineTuneResponse response = openAIClient.retrieveFineTune("ft-pqCLc9Xm7WxQDgTnHW5Jlnax");
+        log.info(response);
+    }
+    @Test
+    void cancelFineTune() {
+        CancelFineTuneResponse response = openAIClient.cancelFineTune("ft-pqCLc9Xm7WxQDgTnHW5Jlnax");
+        log.info(response);
+    }
+    @Test
+    void listFineTuneEvents() {
+        ListFineTuneEventsResponse response = openAIClient.listFineTuneEvents("ft-pqCLc9Xm7WxQDgTnHW5Jlnax");
+        log.info(response);
+    }
+    @Test
+    void deleteFineTuneModel() {
+        DeleteFineTuneModelResponse response = openAIClient.deleteFineTuneModel("curie:ft-personal-2022-12-13-03-56-13");
+        log.info(response);
+    }
+    void createModeration() {
 
-	@Test
-	void retrieveModel(){
-
-		RetrieveModelResponse retrieveModelResponse = openAIClient.retrieveModel("text-davinci-003");
-		log.info(retrieveModelResponse);
-	}
-
-	@Test
-	void createCompletion(){
-		CreateCompletionRequest createCompletionRequest = CreateCompletionRequest
-				.builder()
-				.model("text-davinci-003")
-				.prompt("Say this is a test")
-				.maxTokens(7)
-				.temperature(0.0).build();
-
-		CreateCompletionResponse createCompletionResponse = openAIClient.createCompletion(createCompletionRequest);
-		log.info(createCompletionResponse.getChoices().get(0).getText());
-	}
-
-	@Test
-	void createEdit(){
-		CreateEditRequest request = CreateEditRequest.builder()
-				.model("text-davinci-edit-001")
-				.input("What day of the wek is it?")
-				.instruction("Fix the spelling mistakes")
-				.numberOfEdits(1)
-				.build();
-		CreateEditResponse createEditResponse = openAIClient.createEdit(request);
-
-		log.info(createEditResponse);
-	}
-
-
-	@Test
-	void createImage(){
-		CreateImageRequest request = CreateImageRequest.builder()
-				.prompt("A cute baby sea otter")
-				.numberOfImages(2)
-				.size("1024x1024").build();
-
-		CreateImageResponse response = openAIClient.createImage(request);
-		log.info(response);
-	}
-
-
-	@Test
-	void createImageEdit() throws IOException{
-
-		MultipartFile image = new MockMultipartFile("image.png","image.png", "image/png", new FileInputStream(new File("src/test/resources/image.png")));
-		MultipartFile mask = new MockMultipartFile("mask.png","mask.png", "image/png", new FileInputStream(new File("src/test/resources/mask.png")));
-
-		Assertions.assertNotNull(image);
-		//Assertions.assertNotNull(mask);
-		CreateImageEditRequest request = CreateImageEditRequest.builder()
-				.image(image)
-				.mask(mask)
-				.prompt("A golden apple in the middle of space")
-				.size("1024x1024")
-				.build();
-
-		CreateImageEditResponse response = openAIClient.createImageEdit(request);
-		log.info(response);
-
-
-	}
-
-
-
-	@Test
-	void createImageVariation() throws IOException{
-		MultipartFile image = new MockMultipartFile("image.png","image.png", "image/png", new FileInputStream(new File("src/test/resources/image.png")));
-
-		CreateImageVariationRequest request = CreateImageVariationRequest.builder()
-				.image(image)
-				.n(2)
-				.size("1024x1024")
-				.build();
-
-		CreateImageVariationResponse response =  openAIClient.createdImageVariation(request);
-
-		log.info(response);
-	}
-
-
-	@Test
-	void createEmbeddings(){
-
-		CreateEmbeddingsRequest request = CreateEmbeddingsRequest.builder()
-				.model("text-similarity-babbage-001")
-				.input("The food was delicious and the waiter was in a good mood")
-				.build();
-
-		CreateEmbeddingsResponse response = openAIClient.createEmbeddings(request);
-		log.info(response);
-
-
-	}
-
-	@Test
-	void listFiles(){
-
-		ListFilesResponse response = openAIClient.listFiles();
-
-		log.info(response);
-	}
-
-	@Test
-	void uploadFile() throws IOException{
-		MultipartFile image = new MockMultipartFile("data.jsonl","data.jsonl", "application/x-ndjson", new FileInputStream(new File("src/test/resources/data.jsonl")));
-		UploadFileRequest request = UploadFileRequest.builder()
-				.file(image)
-				.purpose("fine-tune")
-				.build();
-		UploadFilesResponse response = openAIClient.uploadFile(request);
-
-		log.info(response);
-
-	}
-
-
-	@Test
-	void deleteFile(){
-		DeleteFileResponse response = openAIClient.deleteFile("file-qaD6okdiNT7p3siM3zFfGo5Q");
-		log.info(response);
-
-	}
-
-	@Test
-	void retrieveFile(){
-
-		RetrieveFileResponse response = openAIClient.retrieveFile("file-HcNL83etYjsrhpE1HwKMIq8e");
-
-		log.info(response);
-
-	}
-
-	@Test
-	void retrieveFileContent(){
-
-		ResponseEntity<?> response = openAIClient.retrieveFileContent("file-HcNL83etYjsrhpE1HwKMIq8e");
-		log.info(response);
-	}
-
-
-	void createFineTune(){
-
-	}
-
-	void listFineTune(){
-
-	}
-
-	void retrieveFineTune(){
-
-	}
-
-	void cancelFineTune(){
-
-	}
-	void listFineTuneEvents(){
-
-	}
-
-	void deleteFineTuneModel(){
-
-	}
-
-	void createModeration(){
-
-	}
+    }
 }
